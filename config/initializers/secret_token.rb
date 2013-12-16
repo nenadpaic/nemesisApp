4ba9bd7549dd77e25d0511a47629672ccc1a4bd9
Nenad Paic <nenadpaic@sbb.rs>
2013-12-16 21:16:33 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NemesisApp::Application.config.secret_key_base = '08de67d3b46ccd1f063c553ba6015aa0d09f6d765a0b4c9630bd10281af536551ae9a11801589a8847124b04215fe01bc9a4c4e5fa48615d277f9be3a4c9b8d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NemesisApp::Application.config.session_store :cookie_store, key: '_nemesisApp_session'
